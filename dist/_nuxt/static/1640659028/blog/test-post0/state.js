09f28bf2a1d9ffaff4a020cc0fbe4e9372baa6f9
George Mbugua <gmbugua2020@gmail.com>
2022-01-01 18:26:51 -0800

    new build

--- dist/_nuxt/static/1640659028/blog/test-post0/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640659028",layout:"post",error:a,state:{theme:""},serverRendered:true,routePath:"\u002Fblog\u002Ftest-post0",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2eb1fbd7"}}}}(null));
---
+++ dist/_nuxt/static/1641090387/blog/test-post0/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641090387",layout:"post",error:a,state:{theme:""},serverRendered:true,routePath:"\u002Fblog\u002Ftest-post0",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"464e032c"}}}}(null));
